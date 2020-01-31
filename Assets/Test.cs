1f0bb862aad32e5dc3a66476c3efd548306883c8
Derek O'Kelly <derekokelly1997@gmail.com>
2020-01-31 19:48:34 +0000

    Add project to github

---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        string button = "joystick button ";
+        for (int i = 0; i < 12; i++)
+            PrintName(button + i.ToString());   
+    }
+
+    void PrintName(string buttonName)
+    {
+        if (Input.GetKey(buttonName))
+            Debug.Log(buttonName);
+    }
+}
