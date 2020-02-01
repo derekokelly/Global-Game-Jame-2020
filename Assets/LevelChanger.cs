263347173eb13543ecaf7338fbf824e9c2c1f243
Derek O'Kelly <derekokelly1997@gmail.com>
2020-02-01 19:25:47 +0000

    Click to teleport player!

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private int positionToLoad;
+
+    private GameObject Player;
+
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            FadeToLevel(35);
+        }
+    }
+
+    public void FadeToLevel(int position)
+    {
+        positionToLoad = position;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        Player.transform.position = new Vector2(positionToLoad, 0);
+        //SceneManager.LoadScene(positionToLoad);
+    }
+}
