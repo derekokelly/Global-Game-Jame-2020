6bfb986c24a8dc00b357dd1218da33a1573ba557
Derek O'Kelly <derekokelly1997@gmail.com>
2020-02-02 10:56:07 +0000

    fixing teleporting cameras

--- Assets/LevelChanger.cs
+++ Assets/LevelChanger.cs
@@ -14,18 +14,30 @@ public class LevelChanger : MonoBehaviour
 
     private Coroutine lastRoutine;
 
+    public Camera[] cameras;
+    private int currentCameraIndex;
+
+
     void Start()
     {
-        //Player = GameObject.Find("Player");
+        currentCameraIndex = 0;
+
+        for (int i = 1; i < cameras.Length; i++)
+        {
+            cameras[i].gameObject.SetActive(false);
+        }
+
+        if (cameras.Length > 0)
+        {
+            cameras[0].gameObject.SetActive(true);
+            Debug.Log("Camera with name: " + cameras[0].name + ", is now enabled");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetMouseButton(0))
-        //{
-        //    FadeToLevel();
-        //}
+        
     }
 
     public void FadeToLevel()
@@ -58,9 +70,33 @@ public class LevelChanger : MonoBehaviour
                 animator.SetBool("New Bool", true);
                 yield return new WaitForSeconds(1);
                 Player.transform.position = new Vector2(Door.transform.position.x, Door.transform.position.y - 1.5f);
+                NextCamera();
                 break;
             }
             yield return null;
+        }
+    }
+
+    void NextCamera()
+    {
+        currentCameraIndex++;
+        Debug.Log("Z button has been pressed. Switching to the next camera");
+        Debug.Log(currentCameraIndex);
+        Debug.Log(cameras.Length);
+        if (currentCameraIndex < cameras.Length)
+        {
+            Debug.Log("In if");
+            cameras[currentCameraIndex - 1].gameObject.SetActive(false);
+            cameras[currentCameraIndex].gameObject.SetActive(true);
+            Debug.Log("Camera with name: " + cameras[currentCameraIndex].name + ", is now enabled");
+        }
+        else
+        {
+            Debug.Log("In else");
+            cameras[currentCameraIndex - 1].gameObject.SetActive(false);
+            currentCameraIndex = 0;
+            cameras[currentCameraIndex].gameObject.SetActive(true);
+            Debug.Log("Camera with name: " + cameras[currentCameraIndex].name + ", is now enabled");
         }
     }
 }
