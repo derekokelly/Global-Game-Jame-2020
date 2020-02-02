3756c9eef7492fbdba21da62aaaae336d0da989a
Derek O'Kelly <derekokelly1997@gmail.com>
2020-02-02 14:06:57 +0000

    add logs & doors

---
+++ Assets/AudioLog.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class AudioLog : MonoBehaviour
+{
+
+    public Flowchart flowchart;
+    public string block;
+
+    private Coroutine lastRoutine;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            lastRoutine = StartCoroutine(Interact());
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StopCoroutine(lastRoutine);
+        }
+    }
+
+    IEnumerator Interact() { 
+        while (true)
+        {
+            if (Input.GetKeyDown("z"))
+            {
+                StartLog();
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    void StartLog()
+    {
+        flowchart.ExecuteBlock(block);
+    }
+}
+
